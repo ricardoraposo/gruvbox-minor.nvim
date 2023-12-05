a743f2c683c1ad1477597ccb6878fab362f93e8f
Ricardo Raposo <ricardorbxx1@gmail.com>
2023-12-05 11:50:32 -0300

    initial release, it has a lot of bugs

---
+++ colors/gruvbox-minor.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local gruvbox_minor = require("gruvbox")
+gruvbox_minor.setup({})
+EOF
---
+++ lua/gruvbox/palette.lua
@@ -0,0 +1,21 @@
+local palette = {
+	Color0 = "#cc241d",
+	Color1 = "#7c6f64",
+	Color2 = "#d3869b",
+	Color3 = "#b8bb26",
+	Color5 = "#fb4934",
+	Color4 = "#fe8019",
+	Color6 = "#fabd2f",
+	Color7 = "#bdae93",
+	Color8 = "#83a598",
+	Color9 = "#ebdbb2",
+	Color10 = "#928374",
+	Color11 = "#1d2021",
+	Color12 = "#3a3d21",
+	Color13 = "#462724",
+	Color14 = "#2f3f33",
+	Color15 = "#504945",
+	Color16 = "#2c2c2b",
+}
+
+return palette
